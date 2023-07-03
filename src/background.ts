9553173a2e388637711697d0957639b34d41715d
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-07-03 21:09:45 +0200

    Initial crude addon

---
+++ src/background.ts
@@ -0,0 +1,16 @@
+﻿let active = false;
+
+function makeOrange(color: string): void {
+    document.body.style.backgroundColor = color;
+}
+
+chrome.action.onClicked.addListener((tab) => {
+    active = !active;
+    const color = active ? 'orange' : 'white';
+    console.log("Test")
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id ? tab.id : -1},
+        func: makeOrange,
+        args: [color]
+    }).then();
+});
