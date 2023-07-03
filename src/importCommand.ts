b1c4137a43298a6b683c4e7dcd28e8c0001e1482
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-07-03 23:25:45 +0200

    get all decks for testing

--- src/importCommand.ts
+++ src/importCommand.ts
@@ -3,12 +3,29 @@
 export function addImportCommandListener(): void {
 	chrome.commands.onCommand.addListener(command => {
 		if (command === constants.commands.import) {
-			chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-				chrome.tabs.sendMessage(tabs[0].id ?? 0, { message: constants.messages.getSelectedText }, function (response) {
-					console.log(response.data);
-
-				});
-			});
+			importToAnki();
 		}
+	});
+}
+
+function importToAnki(): void {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		chrome.tabs.sendMessage(tabs[0].id ?? 0, { message: constants.messages.getSelectedText }, function (response) {
+			// console.log(response.data);
+			fetch('http://localhost:8765', {
+				method: 'Post',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					'action': 'deckNames',
+					'version': 6
+				}) })
+				.then(response => response.json())
+				.then(data => console.log(data))
+				.catch((error) => {
+					console.error('Error:', error);
+				});
+		});
 	});
 }
