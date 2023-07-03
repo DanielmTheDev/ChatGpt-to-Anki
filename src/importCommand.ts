690919ee8bc9844016c32d0b409498cad3e57d2d
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-07-03 23:38:30 +0200

    Add comments

--- src/importCommand.ts
+++ src/importCommand.ts
@@ -11,21 +11,31 @@ export function addImportCommandListener(): void {
 function importToAnki(): void {
 	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 		chrome.tabs.sendMessage(tabs[0].id ?? 0, { message: constants.messages.getSelectedText }, function (response) {
-			// console.log(response.data);
-			fetch('http://localhost:8765', {
-				method: 'Post',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					'action': 'deckNames',
-					'version': 6
-				}) })
-				.then(response => response.json())
-				.then(data => console.log(data))
-				.catch((error) => {
-					console.error('Error:', error);
-				});
+			// choose from available decks
+			// choose from available tags or create new one
+			// save last selection
+			// add notes
+			const transformedData = (response.data as string).split('\n');
+			console.log(transformedData);
+			sendRequest();
 		});
 	});
+}
+
+function sendRequest() {
+	fetch('http://localhost:8765', {
+		method: 'Post',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			'action': 'deckNames',
+			'version': 6
+		})
+	})
+		.then(response => response.json())
+		.then(data => console.log(data))
+		.catch((error) => {
+			console.error('Error:', error);
+		});
 }
