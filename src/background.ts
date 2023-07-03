6f046faae0bd1d6591877276007cc8cfdb83c0d7
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-07-03 23:03:30 +0200

    extract command module

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,3 @@
-﻿import {constants} from "./constants";
+﻿import {addImportCommandListener} from "./importCommand";
 
-
-chrome.commands.onCommand.addListener(command => {
-    if (command === constants.commands.import) {
-        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id ?? 0, {message: constants.messages.getSelectedText}, function (response) {
-                console.log(response.data);
-            });
-        });
-    }
-});
+addImportCommandListener()
---
+++ src/importCommand.ts
@@ -0,0 +1,14 @@
+﻿import {constants} from "./constants";
+
+export function addImportCommandListener(): void {
+    chrome.commands.onCommand.addListener(command => {
+        if (command === constants.commands.import) {
+            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id ?? 0, {message: constants.messages.getSelectedText}, function (response) {
+                    console.log(response.data);
+
+                });
+            });
+        }
+    });
+}
